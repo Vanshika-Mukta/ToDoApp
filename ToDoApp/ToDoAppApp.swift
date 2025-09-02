45cb13cbc51f67dfbb8dc3cf26322b3883800c81
vanshika <vanshika.mukta@emiratesauction.ae>
2025-09-02 09:38:01 +0400

    Initial Commit

---
+++ ToDoApp/ToDoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoAppApp.swift
+//  ToDoApp
+//
+//  Created by Vanshika Mukta on 02/09/2025.
+//
+
+import SwiftUI
+
+@main
+struct ToDoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
